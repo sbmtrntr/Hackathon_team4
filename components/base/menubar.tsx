f178461086e0e632daa864c28a2a274498e7b9f8
Kenjiro56 <hiroken506@gmail.com>
2025-02-28 16:41:20 +0900

    intro/tabs

--- components/base/menubar.tsx
+++ components/base/menubar.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
-const MenuBar: React.FC = () => {
+type MenuBarProps = {
+  children: React.ReactNode;
+}
+const MenuBar: React.FC<MenuBarProps>= (childcomponents) => {
   return (
-    <div>
-      <h1>MenuBar</h1>
-    </div>
+    <Tabs size='md' variant='enclosed'>
+      <TabList>
+        <Tab>基本情報</Tab>
+        <Tab>マッチング情報</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          {childcomponents.children}
+        </TabPanel>
+        <TabPanel>
+          { childcomponents.children }
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
 
