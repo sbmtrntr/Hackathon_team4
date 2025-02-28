4f43ede062d2ca4f6f067f388d8728d8bebd34f1
Kenjiro56 <hiroken506@gmail.com>
2025-02-28 16:52:07 +0900

    fix/tabs

--- components/base/menubar.tsx
+++ components/base/menubar.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Center } from '@chakra-ui/react'
 
 type MenuBarProps = {
   children: React.ReactNode;
 }
 const MenuBar: React.FC<MenuBarProps>= (childcomponents) => {
   return (
-    <Tabs size='md' variant='enclosed'>
-      <TabList>
-        <Tab>基本情報</Tab>
-        <Tab>マッチング情報</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          {childcomponents.children}
-        </TabPanel>
-        <TabPanel>
-          { childcomponents.children }
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <Center>
+      <Tabs size='md' colorScheme='#CFE7FF' mt={5} maxWidth="480px">
+        <TabList>
+          <Tab>基本情報</Tab>
+          <Tab>マッチング情報</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            {childcomponents.children}
+          </TabPanel>
+          <TabPanel>
+            { childcomponents.children }
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Center>
   );
 }
 
