b4dc1cb2fa68ff17d3fb35d470284eb86c90c929
Kenjiro56 <hiroken506@gmail.com>
2025-03-03 21:27:57 +0900

    feat/setting_button_routing

--- components/header.tsx
+++ components/header.tsx
@@ -2,13 +2,15 @@
 import React from "react";
 import { Center, Image, IconButton } from "@chakra-ui/react";
 import { IoSettingsOutline } from "react-icons/io5";
-import { useRouter } from "next/navigate";
-
-const OpenSettings = () => {
-  console.log('OpenSettings');
-}
+import { useRouter } from "next/navigation";
 
 const Header: React.FC = () => {
+  const router = useRouter();
+
+  const OpenSettings = () => {
+    console.log('OpenSettings');
+    router.push('/userinfo');
+  }
   return (
     <Center>
       <Image src='logo-sample.png' h='5vh' mt='5'/>
