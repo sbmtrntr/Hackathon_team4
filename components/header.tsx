e38b42f5c68eb2d760052e4087b802dafb329ae7
Kenjiro56 <hiroken506@gmail.com>
2025-03-03 21:22:00 +0900

    feat/setting_button

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,23 @@
 'use client';
 import React from "react";
-import { Center, Image, Icon } from "@chakra-ui/react";
+import { Center, Image, IconButton } from "@chakra-ui/react";
 import { IoSettingsOutline } from "react-icons/io5";
+import { useRouter } from "next/navigate";
+
+const OpenSettings = () => {
+  console.log('OpenSettings');
+}
 
 const Header: React.FC = () => {
   return (
     <Center>
       <Image src='logo-sample.png' h='5vh' mt='5'/>
-      <Icon as={IoSettingsOutline} fontSize={20} mt='5'/>
+      <IconButton
+        icon={<IoSettingsOutline/>}
+        fontSize={20} mt='5'
+        aria-label="ユーザ設定"
+        onClick={()=>OpenSettings()}
+      />
     </Center>
   );
 }
