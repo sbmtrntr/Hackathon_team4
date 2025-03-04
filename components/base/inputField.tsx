24ec9f0a3a55d0e5fe5fc224cd4984997e8dc74c
Kenjiro56 <hiroken506@gmail.com>
2025-03-04 11:08:15 +0900

    fix/add_name-value-options-onChange_to_props

--- components/base/inputField.tsx
+++ components/base/inputField.tsx
@@ -1,18 +1,42 @@
 import React from "react";
-import { Input, InputGroup, InputLeftElement, Icon } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement, Icon, Select } from "@chakra-ui/react";
 
 type InputFieldProps = {
   icon: React.ElementType;
   placeholder: string;
+  name: string;
   type: string;
+  value?: string;
+  options?: string[];
+  onChange?: (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
 }
-const InputField: React.FC<InputFieldProps> = ({icon, placeholder, type}) => {
+const InputField: React.FC<InputFieldProps> = ({icon, placeholder, name, type, value, options, onChange}) => {
   return (
     <InputGroup>
       <InputLeftElement pointerEvents="none">
         <Icon as={icon} color="gray.500" />
       </InputLeftElement>
-      <Input placeholder={placeholder} type={type} bg="white" borderRadius="md" />
+
+      {options ? (
+        <Select name={name} value={value} placeholder={placeholder} bg="white" borderRadius="md" onChange={onChange}>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </Select>
+      ) : (
+        <Input
+          placeholder={placeholder}
+          type={type}
+          bg="white"
+          borderRadius="md"
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+
+      )}
     </InputGroup>
   );
 }
