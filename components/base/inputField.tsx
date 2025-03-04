51072ce15f9609993cb510a150ce3b175df7bfaa
Kenjiro56 <hiroken506@gmail.com>
2025-03-04 10:34:41 +0900

    feat/inputField.tsx

---
+++ components/base/inputField.tsx
@@ -0,0 +1,20 @@
+import React, {ReactNode} from "react";
+import { Input, InputGroup, InputLeftElement, Icon } from "@chakra-ui/react";
+
+type InputFieldProps = {
+  icon: React.ElementType;
+  placeholder: string;
+  type: string;
+}
+const InputField: React.FC<InputFieldProps> = ({icon, placeholder, type}) => {
+  return (
+    <InputGroup>
+      <InputLeftElement pointerEvents="none">
+        <Icon as={icon} color="gray.500" />
+      </InputLeftElement>
+      <Input placeholder={placeholder} type={type} bg="white" borderRadius="md" />
+    </InputGroup>
+  );
+}
+
+export default InputField;
