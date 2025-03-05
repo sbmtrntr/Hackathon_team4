e4011af81fcde7c4375dcad53e0457e624b32178
Kenjiro56 <hiroken506@gmail.com>
2025-03-05 12:40:04 +0900

    5人まで表示させられるように変更

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -2,40 +2,62 @@
 import { Center, Button, VStack, Box, Heading, Text, List, ListItem, ListIcon } from "@chakra-ui/react";
 import React from "react";
 
+const users = ["ユーザーA", "ユーザーB", "ユーザーC", "ユーザーD", "ユーザーE"];
 
 const Result: React.FC = () => {
 
-  const handleClick = () => {
-    console.log("Slackで話す");
+  const handleClick = (userName) => {
+    alert(`${userName} と Slack で話すボタンが押されました！`);
+    // 実際にはここで Slack のリンクに飛ばす処理を実装
   };
+
   return (
     <Center mt={10}>
-      <VStack>
+      <VStack spacing={6}>
         <Box
-        maxW="lg"
-        mx="auto"
-        bg="white"
-        boxShadow="lg"
-        borderRadius="lg"
-        p={6}
-        mt={10}
-        textAlign="center"
+          maxW="lg"
+          mx="auto"
+          bg="white"
+          boxShadow="lg"
+          borderRadius="lg"
+          p={6}
+          textAlign="center"
         >
-        <Heading as="h1" size="xl" color="gray.800" borderBottom="2px solid" pb={2}>
-          🎉 マッチング結果 🎉
-        </Heading>
-        <Text fontSize="lg" color="gray.700" mt={4}>
-          あなたにぴったりなユーザを見つけました！
-        </Text>
-        <List bg="gray.100" borderRadius="lg" p={4} boxShadow="md" mt={4}>
-          <ListItem fontSize="xl" fontWeight="semibold" color="gray.800" p={3} borderLeft="4px solid" borderColor="blue.500">
-            ユーザーA
-          </ListItem>
-        </List>
-      </Box>
-        <Button onClick={handleClick} textColor="white" bg="#235180">
-          Slackで話す
-        </Button>
+          <Heading as="h1" size="xl" color="gray.800" borderBottom="2px solid" pb={2}>
+            🎉 マッチング結果 🎉
+          </Heading>
+          <Text fontSize="lg" color="gray.700" mt={4}>
+            あなたにぴったりなユーザーを見つけました！
+          </Text>
+          <List bg="gray.100" borderRadius="lg" p={4} boxShadow="md" mt={4} spacing={3}>
+            {users.map((user) => (
+              <ListItem
+                key={user}
+                fontSize="xl"
+                fontWeight="semibold"
+                color="gray.800"
+                p={3}
+                borderLeft="4px solid"
+                borderColor="blue.500"
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <span>
+                  {user}
+                </span>
+                <Button
+                  onClick={() => handleClick(user)}
+                  textColor="white"
+                  bg="#235180"
+                  size="sm"
+                >
+                  Slackで話す
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </VStack>
     </Center>
   );
